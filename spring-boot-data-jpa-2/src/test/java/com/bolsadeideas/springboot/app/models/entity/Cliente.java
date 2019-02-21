3653c0fe6dc2077bc91d85044dee70b47dde3f45
Unknown <yohalmo.garcia@gmail.com>
2019-02-20 21:23:14 -0600

    Clase Cliente

---
+++ spring-boot-data-jpa-2/src/test/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.app.models.entity;
+
+import java.util.Date;
+
+public class Cliente {
+	private Long id;
+	
+	private String nombre;
+	private String apellido;
+	private String correo;
+	private Date createAt;
+	
+}
