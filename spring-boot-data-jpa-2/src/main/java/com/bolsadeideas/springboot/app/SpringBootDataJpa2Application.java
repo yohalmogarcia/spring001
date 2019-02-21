95acccb4b0d6ed9def2bccfc7779002cfb84476c
Unknown <yohalmo.garcia@gmail.com>
2019-02-20 21:14:43 -0600

    start project

---
+++ spring-boot-data-jpa-2/src/main/java/com/bolsadeideas/springboot/app/SpringBootDataJpa2Application.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpa2Application.class, args);
+	}
+
+}
