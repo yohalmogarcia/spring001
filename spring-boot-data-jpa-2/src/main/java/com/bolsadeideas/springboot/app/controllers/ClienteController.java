cc00b2cfb48c86f2296147d948158b763d4bed17
Unknown <yohalmo.garcia@gmail.com>
2019-03-28 20:31:06 -0600

    Entitys, repository, Controller
    Videos 24 y 25

---
+++ spring-boot-data-jpa-2/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -0,0 +1,24 @@
+package com.bolsadeideas.springboot.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
+
+@Controller
+public class ClienteController {
+
+	@Autowired
+	private IClienteDao clienteDao;
+	
+	@RequestMapping(value="/listar", method=RequestMethod.GET)
+	public String listar(Model model) {
+		model.addAttribute("titulo", "Listado Clientes");
+		model.addAttribute("clientes",clienteDao.findAll());
+		return "listar";
+		
+	}
+}
---
+++ spring-boot-data-jpa-2/src/main/java/com/bolsadeideas/springboot/app/models/dao/IClienteDao.java
@@ -0,0 +1,9 @@
+package com.bolsadeideas.springboot.app.models.dao;
+
+import java.util.List;
+
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+
+public interface IClienteDao {
+	public List<Cliente> findAll();
+}
