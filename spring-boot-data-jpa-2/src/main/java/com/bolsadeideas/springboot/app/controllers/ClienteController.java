b17f5a5058832e39bc57670443a2428882b5d00c
Unknown <yohalmo.garcia@gmail.com>
2019-04-03 21:04:52 -0600

    Agregando Interfaz Service
    -video37

--- spring-boot-data-jpa-2/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa-2/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -8,24 +8,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.app.models.service.IClienteService;
 
 @Controller
 public class ClienteController {
 
 	@Autowired
-	private IClienteDao clienteDao;
+	private IClienteService clienteService;
 	
 	@RequestMapping(value="/listar", method=RequestMethod.GET)
 	public String listar(Model model) {
 		model.addAttribute("titulo", "Listado Clientes");
-		model.addAttribute("clientes",clienteDao.findAll());
+		model.addAttribute("clientes",clienteService.findAll());
 		return "listar";
 		
 	}
@@ -43,7 +42,7 @@ public class ClienteController {
 	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model) {
 		Cliente cliente = null;
 		if(id>0) {
-			cliente = clienteDao.findOne(id);
+			cliente = clienteService.findOne(id);
 		}else {
 			return "redirect:/listar";
 		}
@@ -58,14 +57,14 @@ public class ClienteController {
 			model.addAttribute("titulo","Formulario de Cliente");
 			return "form";
 		}
-		clienteDao.save(cliente);
+		clienteService.save(cliente);
 		return "redirect:listar";
 	}
 	
 	@RequestMapping(value="/eliminar/{id}")
 	public String eliminar(@PathVariable(value="id") Long id ) {
 		if(id>0) {
-			clienteDao.delete(id);
+			clienteService.delete(id);
 		}
 		return "redirect:/listar";
 	}
--- spring-boot-data-jpa-2/src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImpl.java
+++ spring-boot-data-jpa-2/src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImpl.java
@@ -6,7 +6,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
@@ -17,20 +16,17 @@ public class ClienteDaoImpl implements IClienteDao {
 	private EntityManager em;
 	
 	@SuppressWarnings("unchecked")
-	@Transactional(readOnly=true)
 	@Override
 	public List<Cliente> findAll() {		
 		return em.createQuery("from Cliente").getResultList();
 	}
 	
 	@Override
-	@Transactional(readOnly=true)
 	public Cliente findOne(Long id) {
 		return em.find(Cliente.class, id);
 	}
 
 	@Override
-	@Transactional
 	public void save(Cliente cliente) {
 		if(cliente.getId()!=null && cliente.getId()>0) {
 			em.merge(cliente);
@@ -40,7 +36,6 @@ public class ClienteDaoImpl implements IClienteDao {
 	}	
 
 	@Override
-	@Transactional
 	public void delete(Long id) {
 		em.remove(findOne(id));
 	}
